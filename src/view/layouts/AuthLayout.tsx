a01f623b4fea7af1e1d5d71375d4f8c7dffab4c0
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-05 19:32:10 -0400

    Finalizando layout tela de login

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,10 +1,17 @@
+import { Outlet } from 'react-router-dom'
 import illustration from '../../assets/images/illustration.png'
 import { Logo } from '../components/Logo'
 
 export function AuthLayout() {
   return (
     <div className='flex w-full h-full'>
-      <div className='w-1/2 h-full'></div>
+      <div className='w-1/2 h-full flex justify-center items-center flex-col gap-16'>
+        <Logo className='text-gray-600 h-6'/>
+
+        <div className='w-full max-w-md'>
+          <Outlet />
+        </div>
+      </div>
 
       <div className='w-1/2 h-full flex justify-center items-center p-8 relative'>
         <img
@@ -13,8 +20,7 @@ export function AuthLayout() {
         />
 
         <div className='max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px]'>
-          {/* <img src={logo} alt="" /> */}
-          <Logo className='text-teal-900 h-8' />
+          <Logo className='text-teal-900 h-8'/>
           <p className='text-gray-700 font-medium text-base mt-4'>
             Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
           </p>
