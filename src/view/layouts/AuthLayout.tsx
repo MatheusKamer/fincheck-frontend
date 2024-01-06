ccad494eaad137993c9a0004c4a7ee03bc37771f
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-05 20:53:54 -0400

    Criando component Input

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -4,24 +4,24 @@ import { Logo } from '../components/Logo'
 
 export function AuthLayout() {
   return (
-    <div className='flex w-full h-full'>
-      <div className='w-full h-full flex justify-center items-center flex-col gap-16 lg:w-1/2'>
-        <Logo className='text-gray-600 h-6'/>
+    <div className="flex w-full h-full">
+      <div className="w-full h-full flex justify-center items-center flex-col gap-16 lg:w-1/2">
+        <Logo className="text-gray-600 h-6"/>
 
-        <div className='w-full max-w-[504px] px-8'>
+        <div className="w-full max-w-[504px] px-8">
           <Outlet />
         </div>
       </div>
 
-      <div className='w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex'>
+      <div className="w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
         <img
           src={illustration}
-          className='object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]' alt=''
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]" alt=""
         />
 
-        <div className='max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px]'>
-          <Logo className='text-teal-900 h-8'/>
-          <p className='text-gray-700 font-medium text-base mt-4'>
+        <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px]">
+          <Logo className="text-teal-900 h-8"/>
+          <p className="text-gray-700 font-medium text-base mt-4">
             Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
           </p>
         </div>
