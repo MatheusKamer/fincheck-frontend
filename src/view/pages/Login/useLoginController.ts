09c823aba04823dee98ecce173ead4e0d4508492
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-06 22:45:02 -0400

    Criando useLoginController com react-hook-form, hookform resolvers e zod

--- src/view/pages/Login/useLoginController.ts
+++ src/view/pages/Login/useLoginController.ts
@@ -1,14 +1,27 @@
+import { z } from 'zod';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+  email: z.string().min(1, 'Email is required').email('Report a valid email'),
+  password: z.string().min(1, 'Password is required').min(8, 'Password must be 8 characters')
+})
+
+type FormData = z.infer<typeof schema>
 
 export function useLoginController() {
   const {
     register,
     handleSubmit: hookFormHandleSumit,
-  } = useForm();
-
-  const handleSubmit = hookFormHandleSumit((data) => {
-    console.log({data});
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
   });
 
-  return { handleSubmit, register }
+  const handleSubmit = hookFormHandleSumit((data) => {
+    console.log('Chamar API com: ', data);
+
+  });
+
+  return { handleSubmit, register, errors }
 }
