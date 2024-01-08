017b78fce30834b5e10a4524c5668dc9d82f6cca
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-07 21:52:48 -0400

    Criando AuthContext e useAuth para direcionamento das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Router } from './Router'
 import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './app/contexts/AuthContext'
 
 const queryClient = new QueryClient
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <AuthProvider>
+        <Router />
 
-      <Toaster />
+        <Toaster />
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
