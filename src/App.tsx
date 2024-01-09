30fc6a033b12d73af0f3e401702771b6aa104a91
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-08 21:03:46 -0400

    Iniciando a configuração para utilização dos 'useQuerys'

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './app/contexts/AuthContext'
 
-const queryClient = new QueryClient
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 function App() {
   return (
--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -3,6 +3,8 @@ import {
   useCallback,
   useState
 } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { usersService } from '../services/usersService';
 import { localStorageKeys } from '../config/localStorageKeys';
 
 interface AuthContextValue {
@@ -20,15 +22,20 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     return !!storegAccessToken; /* Utilizando !! para transformar em boolean */
   });
 
+  useQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () => usersService.me(),
+  });
+
   const signin = useCallback((accessToken: string) => {
     localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
     setSignedIn(true);
-  }, [])
+  }, []);
 
   const signout = useCallback(() => {
     localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
     setSignedIn(false);
-  }, [])
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signedIn, signin, signout }}>
