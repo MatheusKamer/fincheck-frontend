20e7068a2099fb4aa4bdd222a45f741bf9325a6d
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-11 20:31:43 -0400

    Finalizando o dashboard de accounts

--- src/view/pages/Dashboard/components/Accounts/index.tsx
+++ src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -9,7 +9,8 @@ import { EyeIcon } from "../../../../components/icons/EyeIcon";
 import { useAccountsController } from './useAccountsController';
 
 export function Accounts() {
-  const { sliderState, setSliderState } = useAccountsController();
+  const { sliderState, setSliderState, windowWidth } = useAccountsController();
+
   return (
     <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
       <div>
@@ -28,11 +29,11 @@ export function Accounts() {
         </div>
       </div>
 
-      <div className="flex-1 flex flex-col justify-end">
+      <div className="flex-1 flex flex-col justify-end mt-10">
         <div>
           <Swiper
             spaceBetween={16}
-            slidesPerView={3.1}
+            slidesPerView={windowWidth >= 500 ? 3.1 : 1.2}
             onSlideChange={swiper => {
               setSliderState({
                 isBeginning: swiper.isBeginning,
