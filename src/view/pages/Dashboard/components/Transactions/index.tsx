abd1b468c00b3ecdad5f3084f2c87cd69e1e2116
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-12 21:56:58 -0400

    Criando containers de transações

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -5,10 +5,12 @@ import { FilterIcon } from "../../../../components/icons/FilterIcon";
 import { MONTHS } from "../../../../../app/config/constants";
 import { SliderOption } from "./SliderOption";
 import { TransactionsSliderNavigation } from "./TransactionsSliderNavigation";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
 
 export function Transactions() {
   return (
-    <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10">
+    <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
       <header>
         <div className="flex items-center justify-between">
           <button className="flex items-center gap-2">
@@ -45,8 +47,37 @@ export function Transactions() {
         </div>
       </header>
 
-      <div className="bg-red-500 mt-4">
-        Conteudo
+      <div className="mt-4 space-y-2 flex-1 overflow-y-auto scrollbar">
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="expense"/>
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+              <span className="text-sm text-gray-600">04/04/2004</span>
+            </div>
+          </div>
+
+          <span className="text-red-800 tracking-[-0.5px] font-medium">
+            {formatCurrency(-1000)}
+          </span>
+        </div>
+
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="income"/>
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+              <span className="text-sm text-gray-600">04/04/2004</span>
+            </div>
+          </div>
+
+          <span className="text-green-800 tracking-[-0.5px] font-medium">
+            {formatCurrency(1000)}
+          </span>
+        </div>
+
       </div>
     </div>
   )
