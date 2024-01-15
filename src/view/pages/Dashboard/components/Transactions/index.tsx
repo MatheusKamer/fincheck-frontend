4b3bd8f0792912448d0c201fc96c09a9c1505bfc
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-14 20:51:39 -0400

    Ajustando loading e initialloading

--- src/view/pages/Dashboard/components/Transactions/index.tsx
+++ src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,28 +1,31 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
-import { FilterIcon } from "../../../../components/icons/FilterIcon";
-import { MONTHS } from "../../../../../app/config/constants";
 import { SliderOption } from "./SliderOption";
-import { TransactionsSliderNavigation } from "./TransactionsSliderNavigation";
-import { formatCurrency } from "../../../../../app/utils/formatCurrency";
-import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
-import { cn } from "../../../../../app/utils/cn";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { useTransactionsController } from "./useTransactionsController";
+import { TransactionsSliderNavigation } from "./TransactionsSliderNavigation";
+import { MONTHS } from "../../../../../app/config/constants";
 import { Spinner } from "../../../../components/Spinner";
+import { FilterIcon } from "../../../../components/icons/FilterIcon";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
+import { cn } from "../../../../../app/utils/cn";
+import emptyStateImagem from "../../../../../assets/images/empty-state.svg"
 
 export function Transactions() {
-  const { areValuesVisible, isLoading } = useTransactionsController()
+  const { areValuesVisible, transactions, isLoading, isInitialLoading } = useTransactionsController()
+
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
-      {isLoading &&
+      {isInitialLoading &&
         <div className="h-full w-full flex items-center justify-center">
           <Spinner className="h-10 w-10"/>
         </div>
       }
 
-      {!isLoading &&
+      {!isInitialLoading &&
         <>
           <header>
             <div className="flex items-center justify-between">
@@ -61,41 +64,60 @@ export function Transactions() {
           </header>
 
           <div className="mt-4 space-y-2 flex-1 overflow-y-auto scrollbar">
-            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-              <div className="flex-1 flex items-center gap-3">
-                <CategoryIcon type="expense"/>
-
-                <div>
-                  <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
-                  <span className="text-sm text-gray-600">04/04/2004</span>
-                </div>
+            {isLoading && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <Spinner className="w-10 h-10"/>
               </div>
+            )}
 
-              <span className={cn(
-                "text-red-800 tracking-[-0.5px] font-medium",
-                !areValuesVisible && "blur-md"
-              )}>
-                {formatCurrency(-1000)}
-              </span>
-            </div>
-
-            <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
-              <div className="flex-1 flex items-center gap-3">
-                <CategoryIcon type="income"/>
-
-                <div>
-                  <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
-                  <span className="text-sm text-gray-600">04/04/2004</span>
-                </div>
+            {(!hasTransactions && !isLoading) && (
+              <div className="flex flex-col items-center justify-center h-full">
+                <img src={emptyStateImagem} alt="Empty state" />
+                <p className="text-gray-700">
+                  Não encontramos nenhuma transação!
+                </p>
               </div>
+            )}
 
-              <span className={cn(
-                "text-green-800 tracking-[-0.5px] font-medium",
-                !areValuesVisible && "blur-md"
-              )}>
-                {formatCurrency(1000)}
-              </span>
-            </div>
+            {(hasTransactions && !isLoading) && (
+              <>
+                <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="expense"/>
+
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+                      <span className="text-sm text-gray-600">04/04/2004</span>
+                    </div>
+                  </div>
+
+                  <span className={cn(
+                    "text-red-800 tracking-[-0.5px] font-medium",
+                    !areValuesVisible && "blur-md"
+                  )}>
+                    {formatCurrency(-1000)}
+                  </span>
+                </div>
+
+                <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+                  <div className="flex-1 flex items-center gap-3">
+                    <CategoryIcon type="income"/>
+
+                    <div>
+                      <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+                      <span className="text-sm text-gray-600">04/04/2004</span>
+                    </div>
+                  </div>
+
+                  <span className={cn(
+                    "text-green-800 tracking-[-0.5px] font-medium",
+                    !areValuesVisible && "blur-md"
+                  )}>
+                    {formatCurrency(1000)}
+                  </span>
+                </div>
+              </>
+            )}
           </div>
         </>
       }
