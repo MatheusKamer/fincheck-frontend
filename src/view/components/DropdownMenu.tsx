4d031296ed0f5b01584b20d45495955f6ca65ab2
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-15 20:27:45 -0400

    Estilizando dropdown de 'Sair'

--- src/view/components/DropdownMenu.tsx
+++ src/view/components/DropdownMenu.tsx
@@ -1,4 +1,5 @@
 import * as RdxDropdownMenu from '@radix-ui/react-dropdown-menu';
+import { cn } from '../../app/utils/cn';
 
 export function DropdownMenuRoot({children}: { children: React.ReactNode }) {
   return (
@@ -10,30 +11,51 @@ export function DropdownMenuRoot({children}: { children: React.ReactNode }) {
 
 export function DropdownMenuTrigger({children}: { children: React.ReactNode }) {
   return (
-    <RdxDropdownMenu.Trigger>
+    <RdxDropdownMenu.Trigger className="outline-none">
       {children}
     </RdxDropdownMenu.Trigger>
   )
 }
 
-export function DropdownMenuContent({children}: { children: React.ReactNode }) {
+interface DropdownMenuContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function DropdownMenuContent({children, className}: DropdownMenuContentProps) {
   return (
     <RdxDropdownMenu.Portal>
-      <RdxDropdownMenu.Content>
+      <RdxDropdownMenu.Content
+        className={cn(
+          "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
+          className
+        )}
+      >
         {children}
       </RdxDropdownMenu.Content>
     </RdxDropdownMenu.Portal>
   )
 }
 
-export function DropdownMenuItem({children}: { children: React.ReactNode }) {
+interface DropdownMenuItemProps {
+  children: React.ReactNode;
+  className?: string
+}
+
+export function DropdownMenuItem({children, className}: DropdownMenuItemProps) {
   return (
-    <RdxDropdownMenu.Item>
+    <RdxDropdownMenu.Item
+      className={cn(
+        "min-h-[48px] outline-none flex items-center py-2 p-4 text-sm text-gray-800 hover:bg-gray-50 rounded-2xl transition-colors",
+        className
+      )}
+    >
       {children}
     </RdxDropdownMenu.Item>
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const DropdownMenu = {
   Root: DropdownMenuRoot,
   Trigger: DropdownMenuTrigger,
