554ecd5e27f5b62be9e932f39d5545261655a43b
Matheus Kamer <matheus_kamer@hotmail.com>
2024-01-26 17:06:33 -0400

    Adição de config global 'tailwind.config.js'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+/* global require */
 export default {
   content: [
     "./index.html",
@@ -201,10 +202,10 @@ export default {
       'content-show': 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       'overlay-show': 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
     },
-            contentShow: {
-          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
-          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
-        },
+    contentShow: {
+      from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+      to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+    },
   },
   plugins: [
     require('tailwind-scrollbar')
