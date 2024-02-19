da76f3eaa70b7baa96c1f4245c0819c0c0606264
Matheus Kamer <matheus_kamer@hotmail.com>
2024-02-19 19:45:41 -0400

    fix: fix the word 'Despensa' to 'Despesa'

--- src/view/pages/Dashboard/modals/NewTransactionModal/index.tsx
+++ src/view/pages/Dashboard/modals/NewTransactionModal/index.tsx
@@ -25,7 +25,7 @@ export function NewTransactionModal() {
 
   return (
     <Modal
-      title={isExpense ? 'Nova Despensa' : 'Nova Receita'}
+      title={isExpense ? 'Nova Despesa' : 'Nova Receita'}
       open={isNewTransactionModalOpen}
       onClose={closeNewTransactionModal}
     >
