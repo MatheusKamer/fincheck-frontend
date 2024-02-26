8efd0ee88982c3dadbcc86c668b6a8b42229fb57
Matheus Kamer <matheus_kamer@hotmail.com>
2024-02-25 20:23:11 -0400

    fix: changed the font family of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -206,6 +206,9 @@ export default {
       from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
       to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
     },
+    fontFamily: {
+      sans: ['DM Sans', 'sans-serif']
+    }
   },
   plugins: [
     require('tailwind-scrollbar')
